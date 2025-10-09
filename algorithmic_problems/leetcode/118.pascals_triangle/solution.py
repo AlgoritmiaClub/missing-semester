ad85260a843575c9e131ed0fe3555480037f34b5
DaniloTato <Noentiendo2005@gmail.com>
2025-10-09 08:34:19 -0600

    fix formatting errors with devtools

--- algorithmic_problems/leetcode/118.pascals_triangle/solution.py
+++ algorithmic_problems/leetcode/118.pascals_triangle/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def generate(numRows):
-
         triangle = []
 
         for i in range(numRows):
-            
             row = [1] * (i + 1)
 
             for j in range(1, i):
